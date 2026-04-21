d5473bf7c07942892211759d8592c57215691d88
Leniohp <adm@maximagestao.com>
2026-04-20 21:38:18 -0300

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json({ limit: '2mb' }));
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
   if (req.method === 'OPTIONS') return res.sendStatus(200);
   next();
